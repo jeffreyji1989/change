78cdd0254c4f7ba2a8b71f3e3a60437014504952
Jeffrey <jeffreyji@aliyun.com>
2018-12-16 20:24:24 +0800

    1. 统一日志支持 2. 统一异常支持 3. 对controller的增强

---
+++ change-web/src/main/java/com/jeffrey/change/web/ChangeWebApplication.java
@@ -0,0 +1,15 @@
+package com.jeffrey.change.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+public class ChangeWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChangeWebApplication.class, args);
+	}
+
+}
+
---
+++ change-web/src/test/java/com/jeffrey/change/web/ChangeWebApplicationTests.java
@@ -0,0 +1,17 @@
+package com.jeffrey.change.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChangeWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
